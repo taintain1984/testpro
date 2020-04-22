015456f8f4dee91f5bf90abc26f9dd277ecac0cb
zhitian.zhang <zhangzhi11@163.com>
2020-04-22 15:18:33 +0800

    init repository

---
+++ testpro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testpro
+//
+//  Created by zhitian on 2020/4/19.
+//  Copyright © 2020 YiQiJu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
